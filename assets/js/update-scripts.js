a3b884079e945dd6a9cf1941dbf04e7f79dd22a4
tonang <rizwanaritonang@gmail.com>
2015-05-05 12:32:22 +0700

    #new responsive-navigation style

--- assets/js/update-scripts.js
+++ assets/js/update-scripts.js
@@ -2,7 +2,19 @@
 
 	'use strict';
 
-	// responsive-main-menu
+	// RESPONSIVE NAVIGATION ON SIDEBAR
+	$('#nav-page .wrapper > ul').clone(false).find("ul,li").removeAttr("id").remove(".submenu").appendTo($("#nav-side-left ul"));
+
+	// ACTIVE SIDEBAR ON LEFT
+	$('.nav-btn, .close-nav-side').click( function() {
+		var nav_side = $("#nav-side-left");
+		var wrapper  = $(".wrapper-inner");
+		wrapper.toggleClass("on-active");
+		nav_side.toggleClass("active-left");
+		
+	});
+
+	// responsive-main-menu @deprecated
 	$('#nav-page .wrapper > ul').clone(false).find("ul,li").removeAttr("id").remove(".submenu").appendTo($(".mobile-nav"));
 	
 	$('.nav-btn').on("click", function() {
