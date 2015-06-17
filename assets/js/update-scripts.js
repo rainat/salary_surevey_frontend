abf667da1edc214ddc973cee9f3ab73f033037ab
tonang <rizwanaritonang@gmail.com>
2015-06-17 11:16:54 +0700

    #tooltips #placeholder

--- assets/js/update-scripts.js
+++ assets/js/update-scripts.js
@@ -21,6 +21,25 @@
 			toggle: false
 		});
 	});
+
 	
+	$(".gameplaytips").tooltipster({
+		content: $('<div class="box-tooltipster"><h3 class="title-tooltipster">exact job title</h3><p>Although all salary data is displayed anonymously, your job title will be published to enable users to make relevant comparisons. If you have a very unique and identifiable job title you may wish to generalise this to ensure anonymity.</p></div>'),
+		theme: "tooltipster-salary",	
+		animation: "fade",
+		delay: 50,
+		touchDevices: false,
+		trigger: "hover",
+	});
+
+	$(".jobtips").tooltipster({
+		content: $('<div class="box-tooltipster"><h3 class="title-tooltipster">Job Reference</h3><p>Although all salary data is displayed anonymously, your job title will be published to enable users to make relevant comparisons. If you have a very unique and identifiable job title you may wish to generalise this to ensure anonymity.</p></div>'),
+		theme: "tooltipster-salary",	
+		animation: "fade",
+		delay: 50,
+		touchDevices: false,
+		trigger: "hover",
+	});
 	//$("#sortTable").stacktable();
 })(jQuery);
+
